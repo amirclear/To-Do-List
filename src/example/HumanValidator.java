78cf6470aa45d7fa2624e157db906857ebe422a0
Amir <amirhoseinpanjeh878@gmail.com>
2025-04-03 23:38:24 +0330

    add HumanValidator class

---
+++ src/example/HumanValidator.java
@@ -0,0 +1,23 @@
+package example;
+import db.Entity;
+import db.Validator;
+import example.db.exception.InvalidEntityException;
+
+public class HumanValidator implements Validator {
+
+    @Override
+    public void validate(Entity entity) throws InvalidEntityException {
+        if ( !(entity instanceof example.Human)) {
+            throw new IllegalArgumentException("Invalid entity type: Expected Human");
+        }
+
+        example.Human human = (example.Human) entity;
+
+        if ( human.age < 0) {
+            throw new InvalidEntityException("Age must be positive");
+        }
+        if ( human.name == null || human.name.isEmpty()) {
+            throw new InvalidEntityException("Name cannot be null or empty");
+        }
+    }
+}
