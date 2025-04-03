4dc35e02fcb6bc9ff6545218f8631ec9a774c43a
Amir <amirhoseinpanjeh878@gmail.com>
2025-04-03 12:22:59 +0330

    debug the inherit

--- src/db/exception/EntityNotFoundException.java
+++ src/db/exception/EntityNotFoundException.java
@@ -1,16 +1,15 @@
 package db.exception;
 
-public class EntityNotFoundException extends Exception{
-
-    EntityNotFoundException(){
-        System.out.println("Cannot find entity");
+public class EntityNotFoundException extends RuntimeException {
+    public EntityNotFoundException() {
+        super("Cannot find entity");
     }
 
-    EntityNotFoundException(String message){
+    public EntityNotFoundException(String message) {
         super(message);
     }
 
-    public EntityNotFoundException(int id){
-        System.out.println("Cannot find entity with id= " + id);
+    public EntityNotFoundException(int id) {
+        super("Cannot find entity with id=" + id);
     }
 }
