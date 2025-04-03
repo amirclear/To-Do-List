897f561d0b8f34f916d9b4b4a6dcdb7bbc4a6479
Amir <amirhoseinpanjeh878@gmail.com>
2025-04-03 18:06:38 +0330

    fix 'Main.java'

--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,22 @@
-import db.*;
-import db.exception.*;
+package example;
+
+import db.Database;
+import db.exception.EntityNotFoundException;
 import example.Human;
 
-
 public class Main {
-    public static void main(String[] args) throws EntityNotFoundException {
-        Human[] humans = {
-                new Human("Gholi"),
-                new Human("Jamshid"),
-                new Human("Akbar"),
-        };
+    public static void main(String[] args) {
+        Human ali = new Human("Ali");
 
-        System.out.println("#### Test add method ####");
+        Database.add(ali);
 
-        for (Human h : humans) {
-            System.out.println("Adding " + h.name + " to the database.");
-            Database.add(h);
-        }
-
-        for (Human h : humans) {
-            System.out.println("Id of \"" + h.name + "\" is " + h.id + ".");
-        }
-
-        System.out.println();
-        System.out.println("#### Test get method ####");
-
-        int gholiId = humans[0].id;
-        Human gholi = (Human) Database.get(gholiId);
-
-        System.out.println("successfully got " + gholi.name + " from the database.");
-
-        System.out.println();
-        System.out.println("#### Test update method ####");
-
-        gholi.name = "Gholi Mohammadi";
-        Database.update(gholi);
-
-        Human gholiAgain = (Human) Database.get(gholiId);
-        System.out.println("Updated name: \"" + gholiAgain.name + "\".");
-
-        System.out.println();
-        System.out.println("#### Test delete method ####");
-
-        int jamshidId = humans[1].id;
-        Database.delete(jamshidId);
+        ali.name = "Ali Hosseini";
 
         try {
-            Human jamshid = (Human) Database.get(jamshidId);
+            Human aliFromDatabase = (Human) Database.get(1);
+            System.out.println("ali's name in the database: " + aliFromDatabase.name);
         } catch (EntityNotFoundException e) {
-            System.out.println(e.getMessage());
+            System.out.println("Error: " + e.getMessage());
         }
     }
 }
