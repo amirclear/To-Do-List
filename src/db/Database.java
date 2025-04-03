3ee2e6ae64e73d7556062d442f91f1a46f2778c8
Amir <amirhoseinpanjeh878@gmail.com>
2025-04-03 12:06:32 +0330

    add checking null input

--- src/db/Database.java
+++ src/db/Database.java
@@ -1,21 +1,24 @@
 package db;
 
 import db.exception.EntityNotFoundException;
-
 import java.util.ArrayList;
 
 public class Database {
-
     private static ArrayList<Entity> entities = new ArrayList<>();
     private static int indexId = 1;
 
+    private Database() {}
+
     public static void add(Entity e) {
+        if ( e == null ) {
+            throw new IllegalArgumentException("Entity cannot be null");
+        }
         e.id = indexId++;
         entities.add(e);
     }
 
     public static Entity get(int id) throws EntityNotFoundException {
-        for ( Entity e : entities) {
+        for (Entity e : entities) {
             if (e.id == id) {
                 return e;
             }
@@ -23,14 +26,14 @@ public class Database {
         throw new EntityNotFoundException(id);
     }
 
-    public static void delete(int id) throws EntityNotFoundException{
+    public static void delete(int id) throws EntityNotFoundException {
         Entity e = get(id);
         entities.remove(e);
     }
 
-    public static void update(Entity e) throws EntityNotFoundException{
-        Entity newEntity = get(e.id);
-        entities.set(entities.indexOf(newEntity), e);
+    public static void update(Entity updatedEntity) throws EntityNotFoundException {
+        Entity oldEntity = get(updatedEntity.id);
+        int index = entities.indexOf(oldEntity);
+        entities.set(index, updatedEntity);
     }
-
 }
