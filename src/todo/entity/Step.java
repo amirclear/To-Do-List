dc91d487338a5f8d27fd0f46dfbb825fca4344c7
Amir <amirhoseinpanjeh878@gmail.com>
2025-04-06 23:13:15 +0330

    add some method

--- src/todo/entity/Step.java
+++ src/todo/entity/Step.java
@@ -1,11 +1,15 @@
 package example.todo.entity;
 
+import java.util.Date;
+
 public class Step extends db.Entity {
 
     public enum Status {NotStarted,Completed};
     private String title;
     private Status status;
     private int tastRef;
+    private Date creationDate;
+    private Date lastModificationDate;
     private final int Entity_Code = 10;
 
     public Step (String title,int taskRef) {
@@ -41,6 +45,27 @@ public class Step extends db.Entity {
 
     public void setTastRef(int tastRef) {
         this.tastRef = tastRef;
+    }
+
+    @Override
+    public String toString() {
+        return "Title : " + getTitle() + "\nID : " + id + "\nStatus : " + getStatus();
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Date getLastModificationDate() {
+        return lastModificationDate;
+    }
+
+    public void setLastModificationDate(Date lastModificationDate) {
+        this.lastModificationDate = lastModificationDate;
     }
 
 }
