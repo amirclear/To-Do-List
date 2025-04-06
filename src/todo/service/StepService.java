47c99c70b0009baafc0722e28068ee647c128364
Amir <amirhoseinpanjeh878@gmail.com>
2025-04-06 23:16:22 +0330

    add StepService.java

--- src/todo/service/StepService.java
+++ src/todo/service/StepService.java
@@ -1,4 +1,23 @@
 package example.todo.service;
 
+import example.db.exception.InvalidEntityException;
+import example.todo.entity.Step;
+
+import java.util.Scanner;
+
 public class StepService {
+
+    public static void saveStep(int taskRef, String title) throws InvalidEntityException {
+        System.out.println("Enter the ID number of Task to add Step : ");
+        Scanner sc = new Scanner(System.in);
+        int taskID = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Enter the Title of Step : ");
+        String stepTitle = sc.nextLine();
+
+        Step step = new Step(stepTitle,taskID);
+        db.Database.add(step);
+        System.out.println("Step Added\nID : " + taskID);
+    }
+
 }
