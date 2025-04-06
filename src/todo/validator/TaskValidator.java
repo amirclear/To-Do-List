3aa3efe86a10f6a8b0697aff71ba96304bb18215
Amir <amirhoseinpanjeh878@gmail.com>
2025-04-06 10:06:54 +0330

    add TaskValidator.java

--- src/todo/validator/TaskValidator.java
+++ src/todo/validator/TaskValidator.java
@@ -1,4 +1,22 @@
 package example.todo.validator;
 
-public class TaskValidator {
+import db.Entity;
+import example.db.exception.InvalidEntityException;
+import example.todo.entity.Task;
+
+public class TaskValidator implements db.Validator {
+
+    @Override
+    public void validate(Entity entity) throws InvalidEntityException {
+        if ( entity instanceof Task) {
+            Task task = (Task) entity;
+            if (task.getTitle() == null ||  task.getTitle().isEmpty()) {
+                throw new IllegalArgumentException("Task title can't be empty or null");
+            }
+        }
+        else {
+            throw new IllegalArgumentException("Input is not a Task");
+        }
+    }
+
 }
