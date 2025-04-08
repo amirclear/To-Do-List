17a3fd4e7f96ed0194fb237629011293e1fdcec3
Amir <amirhoseinpanjeh878@gmail.com>
2025-04-08 13:35:23 +0330

    fix constructor method

--- src/db/Entity.java
+++ src/db/Entity.java
@@ -4,8 +4,8 @@ public abstract class Entity implements Cloneable {
     public int id;
 
     @Override
-    protected Object clone() throws CloneNotSupportedException{
-        return super.clone();
+    protected Entity clone() throws CloneNotSupportedException{
+        return (Entity) super.clone();
     }
 
     public abstract int getEntityCode();
